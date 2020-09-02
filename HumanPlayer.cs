4e3716dbee83a6e1c09334372a23141bee4cb520
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-09-02 15:00:01 -0500

    added a human player that collects user input

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
-
+        public override int Roll()
+        {
+            Console.WriteLine("Your Turn!");
+            Console.Write($"Enter a number between 1 and {DiceSize}: ");
+            string strInput = Console.ReadLine();
+            int Input = Int32.Parse(strInput);
+            // Return a random number between 1 and DiceSize
+            return Input;
+        }
     }
 }
